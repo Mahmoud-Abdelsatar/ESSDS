08664e8749e81c9b398872683e96f1642bab3469
Your Name <you@example.com>
2025-11-14 16:35:30 -0500

    static generators of G1 and G2 update

--- demo/sp_demo.cpp
+++ demo/sp_demo.cpp
@@ -39,7 +39,7 @@ bool send_reenc_key_to_edge(const std::vector<uint8_t>& rk_ser) {
     // Placeholder: actual network code to send rk_ser to edge server
     std::cout << "Sending re-encryption key to edge server, size = " << rk_ser.size() << " bytes." << std::endl;
     // e.g., open TCP connection, send data, close connection
-    int sock=SocketManager::connectTo("127.0.0.1", 5000);
+    int sock=SocketManager::connectTo("192.168.1.3", 5000);
     SocketManager::sendData(sock, rk_ser);
     SocketManager::closeSock(sock);
     return true;
@@ -48,7 +48,7 @@ bool send_ct_to_edge(const std::vector<uint8_t>& ct_ser) {
     // Placeholder: actual network code to send ct_ser to edge server
     std::cout << "Sending ciphertext to edge server, size = " << ct_ser.size() << " bytes." << std::endl;
     // e.g., open TCP connection, send data, close connection
-    int sock=SocketManager::connectTo("127.0.0.1", 5000);
+    int sock=SocketManager::connectTo("192.168.1.3", 5000);
     SocketManager::sendData(sock, ct_ser);
     std::vector<uint8_t> ack=SocketManager::recvData(sock);
     SocketManager::closeSock(sock);
@@ -77,51 +77,52 @@ bool export_public_key(const std::vector<uint8_t>& spparams_ser)
     SocketManager::closeSock(srcok);
     return true;
 }
-std::string to_hex(const std::vector<uint8_t>& data) {
-    std::ostringstream oss;
-    for (uint8_t b : data) {
-        oss << std::hex << std::setw(2) << std::setfill('0') << (int)b;
-    }
-    return oss.str();
-}
-void print_relic_curves_paramters()
-{
-    ep_t g1; ep_null(g1); ep_new(g1); ep_curve_get_gen(g1);
-    ep2_t g2;ep2_null(g2);ep2_new(g2); ep2_curve_get_gen(g2);
-    gt_t gt; gt_null(gt); gt_new(gt);
-    pc_map(gt,g1,g2); 
-    size_t ep_len=ep_size_bin(g1,1);
-    ssize_t ep2_len=ep2_size_bin(g2,1);
-    size_t gt_len=gt_size_bin(gt,1);
-    std::cout<<"|G1|="<<ep_len<<std::endl;
-    std::cout<<"|G2|"<<ep2_len<<std::endl;
-    std::cout<<"|G_T|"<<gt_len<<std::endl;
-    std::vector<uint8_t> g1_ser=ps::serialize_ep(g1);
-    std::vector<uint8_t> g2_ser=ps::serialize_ep2(g2);
-    std::vector<uint8_t> gt_ser=ps::serialize_gt(gt);
-    std::cout << "G1 hex: " << to_hex(g1_ser) << std::endl;
-    std::cout << "G2 hex: " << to_hex(g2_ser) << std::endl;
-    std::cout << "GT hex: " << to_hex(gt_ser) << std::endl;
-}
-void print_relic_curves_static_paramters()
-{
-    ep_t g1; ep_null(g1); ep_new(g1); ps::get_static_g1(g1);
-    ep2_t g2;ep2_null(g2); ep2_new(g2); ps::get_static_g2(g2);
-    gt_t gt; gt_null(gt); gt_new(gt);
-    pc_map(gt,g1,g2); 
-    size_t ep_len=ep_size_bin(g1,1);
-    ssize_t ep2_len=ep2_size_bin(g2,1);
-    size_t gt_len=gt_size_bin(gt,1);
-    std::cout<<"|G1|="<<ep_len<<std::endl;
-    std::cout<<"|G2|"<<ep2_len<<std::endl;
-    std::cout<<"|G_T|"<<gt_len<<std::endl;
-    std::vector<uint8_t> g1_ser=ps::serialize_ep(g1);
-    std::vector<uint8_t> g2_ser=ps::serialize_ep2(g2);
-    std::vector<uint8_t> gt_ser=ps::serialize_gt(gt);
-    std::cout << "G1 hex: " << to_hex(g1_ser) << std::endl;
-    std::cout << "G2 hex: " << to_hex(g2_ser) << std::endl;
-    std::cout << "GT hex: " << to_hex(gt_ser) << std::endl;
-}
+// std::string to_hex(const std::vector<uint8_t>& data) {
+//     std::ostringstream oss;
+//     for (uint8_t b : data) {
+//         oss << std::hex << std::setw(2) << std::setfill('0') << (int)b;
+//     }
+//     return oss.str();
+// }
+// Debug the curves paramters
+// void print_relic_curves_paramters()
+// {
+//     ep_t g1; ep_null(g1); ep_new(g1); ep_curve_get_gen(g1);
+//     ep2_t g2;ep2_null(g2);ep2_new(g2); ep2_curve_get_gen(g2);
+//     gt_t gt; gt_null(gt); gt_new(gt);
+//     pc_map(gt,g1,g2); 
+//     size_t ep_len=ep_size_bin(g1,1);
+//     ssize_t ep2_len=ep2_size_bin(g2,1);
+//     size_t gt_len=gt_size_bin(gt,1);
+//     std::cout<<"|G1|="<<ep_len<<std::endl;
+//     std::cout<<"|G2|"<<ep2_len<<std::endl;
+//     std::cout<<"|G_T|"<<gt_len<<std::endl;
+//     std::vector<uint8_t> g1_ser=ps::serialize_ep(g1);
+//     std::vector<uint8_t> g2_ser=ps::serialize_ep2(g2);
+//     std::vector<uint8_t> gt_ser=ps::serialize_gt(gt);
+//     std::cout << "G1 hex: " << to_hex(g1_ser) << std::endl;
+//     std::cout << "G2 hex: " << to_hex(g2_ser) << std::endl;
+//     std::cout << "GT hex: " << to_hex(gt_ser) << std::endl;
+// }
+// void print_relic_curves_static_paramters()
+// {
+//     ep_t g1; ep_null(g1); ep_new(g1); ps::get_static_g1(g1);
+//     ep2_t g2;ep2_null(g2); ep2_new(g2); ps::get_static_g2(g2);
+//     gt_t gt; gt_null(gt); gt_new(gt);
+//     pc_map(gt,g1,g2); 
+//     size_t ep_len=ep_size_bin(g1,1);
+//     ssize_t ep2_len=ep2_size_bin(g2,1);
+//     size_t gt_len=gt_size_bin(gt,1);
+//     std::cout<<"|G1|="<<ep_len<<std::endl;
+//     std::cout<<"|G2|"<<ep2_len<<std::endl;
+//     std::cout<<"|G_T|"<<gt_len<<std::endl;
+//     std::vector<uint8_t> g1_ser=ps::serialize_ep(g1);
+//     std::vector<uint8_t> g2_ser=ps::serialize_ep2(g2);
+//     std::vector<uint8_t> gt_ser=ps::serialize_gt(gt);
+//     std::cout << "G1 hex: " << to_hex(g1_ser) << std::endl;
+//     std::cout << "G2 hex: " << to_hex(g2_ser) << std::endl;
+//     std::cout << "GT hex: " << to_hex(gt_ser) << std::endl;
+// }
 int main() {
     ps::init_relic();
     // ps::set_static_generators();
@@ -129,8 +130,8 @@ int main() {
     sp.init_keys();
     std::cout << "SP initialized." << std::endl;
     // cout the Relic setup such as g1 and g2 as well as the sizes of lengths of G1 G2 and Gt
-    print_relic_curves_paramters();   
-    print_relic_curves_static_paramters();   
+    // print_relic_curves_paramters();   
+    // print_relic_curves_static_paramters();   
      
     // ps::GlobalParams gp;
     // // sp.export_global_params(gp);
@@ -198,37 +199,5 @@ int main() {
         }
     } while (n!=-1);
     
-    // // SP generates reenc key
-    // auto rk_ser = sp.generate_reenc_key();
-    // std::cout << "SP generated re-encryption key, size = " << rk_ser.size() << " bytes." << std::endl;  
-    // // SP opens tcp connection to edge and sends rk_ser
-    // int n;
-    // std::cin >> n;
-    
-    // // Placeholder: actual network code omitted for brevity
-
-    // send_reenc_key_to_edge(rk_ser);
-
-    // std::cin >> n;
-    // //SP signcrypts a message
-    // std::string message = "Hello, this is a secret message.";
-    // std::vector<uint8_t> mvec(message.begin(), message.end());
-    // uint64_t SN = 1;
-    // auto ct_ser = sp.signcrypt(mvec, SN);
-    // std::cout << "SP signcrypted message, CT size = " << ct_ser.size() << " bytes." << std::endl;   
-    
-    // std::cin >> n;
-    // // SP opens tcp connection to the edge and sends ct_ser
-    // // Placeholder: actual network code omitted for brevity
-    // std::cout << "SP sent ciphertext to edge server." << std::endl; 
-    // send_ct_to_edge(ct_ser);
-
-    // std::cin >> n;
-    // // SP prepares group secret material for vehicle
-    // auto gsm_ser = sp.generate_group_material_serialized_for_vehicle();
-    // std::cout << "SP generated group secret material for vehicle, size = " << gsm_ser.size() << " bytes." << std::endl;  
-    // // SP opens tcp connection to vehicle and sends gsm_ser
-    // std::cin >> n;  
-    // send_group_material_to_vehicle(gsm_ser);
     return 0;
 }
